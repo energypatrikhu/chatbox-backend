a7ddca9581dde0c566bd0593250a3183453aed79
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-02 12:21:58 +0200

    Szoba üzenetek lekérésének implementálása

---
+++ src/routes/chat/messages.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import prisma from '../../libs/prisma';
+
+const router = Router();
+
+router.get('/:roomType/:roomId', async (req, res) => {
+	const { roomType, roomId } = req.params as {
+		roomType: 'contact' | 'group';
+		roomId: string;
+	};
+
+	if (!roomType || !roomId) {
+		return res.status(400).json({
+			success: false,
+			error: 'Érvénytelen szoba típus vagy azonosító',
+		});
+	}
+
+	const messages = await prisma.group.findMany({
+		where: {
+			id: parseInt(roomId),
+			type: roomType,
+		},
+		select: {
+			Messages: {
+				include: {
+					User: true,
+				},
+			},
+		},
+		take: 100,
+		orderBy: {
+			createdAt: 'desc',
+		},
+	});
+
+	res.json({
+		success: true,
+		data: messages,
+	});
+});
+
+export default router;
