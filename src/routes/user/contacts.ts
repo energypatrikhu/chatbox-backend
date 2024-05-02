92fe8cec135ac8ede82c24efad65cc05dc9acbba
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-02 12:19:35 +0200

    Felhasználó csoportjai módosítás implementálása

--- src/routes/user/contacts.ts
+++ src/routes/user/contacts.ts
@@ -73,4 +73,111 @@ router.get('/:userId', async (req, res) => {
 	});
 });
 
+router.patch('/:userId', async (req, res) => {
+	const { userId } = req.params as {
+		userId?: string;
+	};
+	const { loginId } = req.query as {
+		loginId?: string;
+	};
+	const { contactIds } = req.body as {
+		contactIds?: number[];
+	};
+
+	if (!userId) {
+		return res.status(400).json({
+			success: false,
+			error: 'Érvénytelen felhasználói azonosító',
+		});
+	}
+
+	if (!loginId) {
+		return res.status(400).json({
+			success: false,
+			error: 'Érvénytelen bejelentkezési azonosító',
+		});
+	}
+
+	if (!contactIds || !Array.isArray(contactIds)) {
+		return res.status(400).json({
+			success: false,
+			error: 'Érvénytelen kapcsolat azonosítók',
+		});
+	}
+
+	const _userId = parseInt(userId);
+
+	const checkLoginId = await prisma.login.findUnique({
+		where: {
+			loginId,
+		},
+	});
+
+	if (!checkLoginId) {
+		return res.status(404).json({
+			success: false,
+			error: 'Nem található',
+		});
+	}
+
+	if (checkLoginId.userId !== _userId) {
+		return res.status(404).json({
+			success: false,
+			error: 'Nem található',
+		});
+	}
+
+	const userContacts = await prisma.user.findFirst({
+		where: {
+			id: _userId,
+		},
+		select: {
+			Contacts: {
+				select: {
+					id: true,
+				},
+			},
+		},
+	});
+
+	if (!userContacts) {
+		return res.status(404).json({
+			success: false,
+			error: 'Nem található',
+		});
+	}
+
+	const contactIdsSet = new Set(contactIds);
+	const userContactIdsSet = new Set(
+		userContacts.Contacts.map((contact) => contact.id),
+	);
+
+	const addContacts = contactIds.filter(
+		(contactId) => !userContactIdsSet.has(contactId),
+	);
+	const removeContacts = userContacts.Contacts.filter(
+		(contact) => !contactIdsSet.has(contact.id),
+	);
+
+	await prisma.user.update({
+		where: {
+			id: _userId,
+		},
+		data: {
+			Contacts: {
+				connect: addContacts.map((contactId) => ({
+					id: contactId,
+				})),
+				disconnect: removeContacts.map((contact) => ({
+					id: contact.id,
+				})),
+			},
+		},
+	});
+
+	res.status(200).json({
+		success: true,
+	});
+});
+
 export default router;
