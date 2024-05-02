69b0d83c0accdd5a2f286b36fce8e265e93d4afe
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-02 18:28:02 +0200

    room join

---
+++ src/socket/handlers/handleJoin.ts
@@ -0,0 +1,8 @@
+import type { Socket } from 'socket.io';
+
+export default function handleJoin(socket: Socket) {
+	socket.on('join', (room) => {
+		socket.join(room);
+		console.log(`[${socket.id}] joining ${room}`);
+	});
+}
