aa53361612c06d940a341372abdeaa15dc36d217
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-02 12:18:06 +0200

    Csoport kontroller implementálása

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import checkRouter from '../routes/user/check';
 import contactsRouter from '../routes/user/contacts';
+import groupsRouter from '../routes/user/groups';
 import dataRouter from '../routes/user/data';
 import loginRouter from '../routes/user/login';
 import logoutRouter from '../routes/user/logout';
@@ -11,6 +12,7 @@ const router = Router();
 
 router.use('/check', checkRouter);
 router.use('/contacts', contactsRouter);
+router.use('/groups', groupsRouter);
 router.use('/data', dataRouter);
 router.use('/login', loginRouter);
 router.use('/logout', logoutRouter);
