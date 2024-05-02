5b3988fb95061cf14aa7bf07d73a59d2a635bd81
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-02 18:30:17 +0200

    Chat üzenetek kezelése

--- src/socket/handlers/handleMessages.ts
+++ src/socket/handlers/handleMessages.ts
@@ -1,14 +1,62 @@
 import type { Server, Socket } from 'socket.io';
 import type { Message } from '../../types/socketIo';
+import prisma from '../../libs/prisma';
 
 export default function handleMessages(io: Server, socket: Socket) {
 	socket.on('message', async (message: Message) => {
+		console.log('Received message', message);
+
+		if (!message.destinationType || !message.destinationId) {
+			console.log('Invalid message');
+			return;
+		}
+
+		const updateData = await prisma.group.update({
+			where: {
+				id: parseInt(message.destinationId),
+				type: message.destinationType,
+			},
+			data: {
+				Messages: {
+					create: {
+						User: {
+							connect: {
+								id: parseInt(message.senderId),
+							},
+						},
+						text: message.text,
+					},
+				},
+			},
+			include: {
+				Messages: {
+					include: {
+						User: {
+							select: {
+								id: true,
+								name: true,
+							},
+						},
+						Group: {
+							select: {
+								id: true,
+								type: true,
+							},
+						},
+					},
+					take: 1,
+					orderBy: {
+						createdAt: 'desc',
+					},
+				},
+			},
+		});
+
+		updateData.Messages;
+
 		io.to(`${message.destinationType}:${message.destinationId}`).emit(
 			'message',
-			{
-				senderId: message.senderId,
-				text: message.text,
-			},
+			updateData.Messages[0],
 		);
 	});
 }
