59bf34d67de5684dd8b4a6d13bd29a1279bf99ca
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-02 12:24:25 +0200

    SocketIO felhasználó hitelesítés implementálása

---
+++ src/socket/handlers/authSocket.ts
@@ -0,0 +1,38 @@
+import type { Socket } from 'socket.io';
+import prisma from '../../libs/prisma';
+
+export default async function authSocket(socket: Socket) {
+	const { userId } = socket.handshake.auth as {
+		userId?: number;
+	};
+
+	console.log('Authenticating socket');
+
+	if (!userId) {
+		console.log('No user id');
+		return socket.disconnect();
+	}
+
+	const user = await prisma.user.findUnique({
+		where: {
+			id: userId,
+		},
+		include: {
+			Groups: true,
+		},
+	});
+
+	if (!user) {
+		console.log('No user');
+		return socket.disconnect();
+	}
+
+	console.log('Socket authenticated, joining groups');
+
+	for (const group of user.Groups) {
+		socket.join(`${group.type}:${group.id}`);
+		console.log(`[${socket.id}] joining ${group.type}:${group.id}`);
+	}
+
+	return user;
+}
